d731173dc592861971f27a31513fb95140ba0e24
Ali <badmad93@live.com>
2021-04-25 23:58:23 -0700

    error corrections

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,16 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'bundle.js'
     },
-    // module: {
-    //     rules: [{
-    //         test: /\.m?js$/,
-    //         exclude: /node_modules/,
-    //         use: {
-    //             loader: 'babel-loader',
-    //             options: {
-    //                 presets: ['@babel/preset-env']
-    //             }
-    //         }
-    //     }]
-    // }
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
 }
